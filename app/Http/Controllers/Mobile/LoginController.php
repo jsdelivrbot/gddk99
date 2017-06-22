b1a2ace8192bffa23141758f527f26e81fecb3b0
陈小龙 <403228075@qq.com>
2017-06-22 14:10:02 +0800

    修复

--- app/Http/Controllers/Mobile/LoginController.php
+++ app/Http/Controllers/Mobile/LoginController.php
@@ -12,7 +12,7 @@ class LoginController extends Controller
     protected $config = [
         'wechat' => [
             'client_id'     => 'wx9f3dd1dd7cc72602',
-            'client_secret' => ['b2600888426c904583800ac5a9de4a8f','CWI4y86blVB8OhUQg4BnMF'],
+            'client_secret' => 'b2600888426c904583800ac5a9de4a8f',
             'redirect'      => 'http://www.gddk99.com/mobile/wx-callback',
         ]
     ];
