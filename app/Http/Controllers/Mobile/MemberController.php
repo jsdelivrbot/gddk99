d16ec2ba2e65383f9b9def58fb3b4b0377f4a8f8
陈小龙 <13480731740@qq.com>
2017-07-20 20:33:18 +0800

    修复

--- app/Http/Controllers/Mobile/MemberController.php
+++ app/Http/Controllers/Mobile/MemberController.php
@@ -131,7 +131,7 @@ class MemberController extends Controller
 
         // 判断图片是否存在，不存在生成图片---取反
         if(!file_exists($poster)){
-            $common->Poster($common->picUrlPath('hbbg.png'),$common->PublicPath('qrcode',$memberId),$common->PublicPath('sc',$memberId),$dst_x='190',$dst_y='370');
+            $common->Poster($common->picUrlPath('hbbg.png'),$common->PublicPath('qrcode',$memberId),$common->PublicPath('sc',$memberId),$dst_x='226',$dst_y='370');
         }
 
         // 获取图片输出到前端界面
